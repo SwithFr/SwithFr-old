cba44912db2f33eeb87b892c05f1090784a6f098
SwithFr <contact@swith.fr>
2015-09-22 14:54:26 +0200

    Send mail

--- App/Controllers/PagesController.php
+++ App/Controllers/PagesController.php
@@ -22,6 +22,28 @@ class PagesController extends AppController
         $d['metas'] = $this->Page->getFirst([
             "where" => ['ref' => 'contact']
         ]);
+        
+        $this->loadModel("Contact");
+        if ($this->Request->isPost) {
+            $d['posted'] = $this->Request->data;
+            if ($this->Contact->validate($d['posted'])) {
+                $subject = isset($d['posted']->subject) ? ' : ' . $d['posted']->subject : '';
+                $this->loadComponent("Mailer");
+                $this->Mailer->addAddress($_ENV['MAIL_USERNAME']);
+                $this->Mailer->Subject = "Swith:: Un nouveau message" . $subject;
+                $this->Mailer->Body = $d['posted']->msg;
+                $this->Mailer->isHTML(true);
+                if (!$this->Mailer->send()) {
+                    $this->Session->setFlash($this->Mailer->ErrorInfo, 'error');
+                } else {
+                    $this->Session->setFlash("Votre message à bien été envoyé");
+                    $this->redirect($this->Request->referer);
+                }
+            } else {
+                $d['errors'] = $this->Contact->getErrors();
+                $this->Session->setFlash("Veuillez verifier vos informations", 'error');
+            }
+        }
 
         return $this->set($d);
     }
