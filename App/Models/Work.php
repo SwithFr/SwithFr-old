45c98692d86d9f09aff7816b49f92303eda80782
SwithFr <contact@swith.fr>
2015-09-22 13:51:31 +0200

    work model

--- App/Models/Work.php
+++ App/Models/Work.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 
+use Core\Helpers\Text;
+use Core\Models\Behaviors\Validator;
+
 class Work extends AppModel
 {
 
+    use Validator;
+
     public $primaryKey = "slug";
+
+    public $validationRules = [
+        "title" => [
+          ['ruleName' => 'required', 'message' => 'Titre obligatoire']
+        ],
+        "description" => [
+            ['ruleName' => 'required', 'message' => 'Description obligatoire']
+        ]
+    ];
+
+    public function beforeSave($data)
+    {
+        if (isset($data->title) && !empty($data->title)) {
+            $data->slug = Text::toSlug($data->title);
+        }
+
+        return $data;
+    }
 }
