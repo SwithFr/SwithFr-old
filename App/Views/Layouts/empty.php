2498d04fd343ac2d99ec829e61062e77341f982f
SwithFr <contact@swith.fr>
2015-09-23 13:57:00 +0200

    fix lang and noindex

--- App/Views/Layouts/empty.php
+++ App/Views/Layouts/empty.php
@@ -1,10 +1,11 @@
 <?php use App\Config\App; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0">
     <meta name="author" content="Jeremy Smith"/>
+    <meta name="robots" content="noindex">
     <title>
         Connect |
         <?= App::getInstance()->getAppSettings('name'); ?>
