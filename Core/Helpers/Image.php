a398701d445d6bfe7b07f9656ca71ab19abfa03c
SwithFr <contact@swith.fr>
2015-09-23 17:25:41 +0200

    add some method

--- Core/Helpers/Image.php
+++ Core/Helpers/Image.php
@@ -16,4 +16,23 @@ class Image
         return '.' . @end(explode('.',$imageName));
     }
 
+    public static function getWidth($imageName)
+    {
+        return self::getSize($imageName)[0];
+    }
+
+    public static function getHeight($imageName)
+    {
+        return self::getSize($imageName)[1];
+    }
+
+    public static function getHtmlAttr($imageName){
+        return self::getSize($imageName)[3];
+    }
+
+    public static function getSize($imageName)
+    {
+        return getimagesize(BASE . DS . 'App' . DS . 'Webroot' . DS . 'img' . DS . $imageName);
+    }
+
 } 
